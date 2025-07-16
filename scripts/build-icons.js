580a487d55830f0619c593d9c5230f18c88a3348
awers24 <83694881+awers24@users.noreply.github.com>
2025-07-16 21:07:52 +0300

    add onboarding icons

--- scripts/build-icons.js
+++ scripts/build-icons.js
@@ -3,5 +3,5 @@ const { generateIcons } = require('@vkontakte/icons-scripts');
 generateIcons({
   srcDirectory: './src',
   distDirectory: './dist',
-  extraCategories: ['100', '106', '120', '126', '300', 'Unsorted'],
+  extraCategories: ['100', '106', '120', '126', '300', 'Onboarding', 'Unsorted'],
 });
